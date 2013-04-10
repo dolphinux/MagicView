7d43dc41c114cc8dbd90a9e194dba0ffedb0a865
dolphinux <shuyan@yiduimac.local>
2013-04-10 10:26:18 +0800

    create project

---
+++ MAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAAppDelegate.h
+//  MagicView
+//
+//  Created by dolphinux	 on 13-4-10.
+//  Copyright (c) 2013年 dolphinux	. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
